9c71f0ea9d7e80521e9fafa9db23d6ba4eae5632
dejauls <rajauls178@gmail.com>
2024-09-17 01:07:20 +0700

    baru

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], // Replace 'sans' with your preferred key if needed
+      },
+    },
+  },
+  plugins: [],
+}
