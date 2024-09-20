e19a669b227f03f99f206fbc0e01bc391c6c0f45
dejauls <rajauls178@gmail.com>
2024-09-20 09:38:57 +0700

    sedikit tambahan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'
+  content: ['./index.html', './about.html', './brosur.html', './produk.html', './artikel.html', './detail-artikel.html'
   ],
   theme: {
     extend: {
