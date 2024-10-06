6ec258fb99ad528db7b06bd79cc653c9afa4ef77
dejauls <rajauls178@gmail.com>
2024-10-06 13:05:10 +0700

    perbaiki artikel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './about.html', './brosur.html', './produk.html', './artikel.html', './detail-artikel.html', './produkdetail.html', './kontak.html'
+  content: ['./index.html', './about.html', './brosur.html', './produk.html',
+     './artikel.html', './detail-artikel.html', './produkdetail.html', './kontak.html',
+     './pupuk-terbaik-untuk-hasil-panen-yang-optimal-di-Sumatera-Utara.html', './apakah-pupuk-aman-untuk-sayuran.html'
   ],
   theme: {
     extend: {
       colors: {
         'custom-green': '#273913',
         'greeno': '#008000',
-        'putih': '#fffef4', 
+        'putih': '#fffef4',
       },
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'], 
+        sans: ['Poppins', 'sans-serif'],
       },
     },
   },
