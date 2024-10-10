62facc4139c62c5208e4063086e0e5ad617ad411
dejauls <rajauls178@gmail.com>
2024-10-10 21:49:14 +0700

    sa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './about.html', './brosur.html', './produk.html',
-     './artikel.html', './detail-artikel.html', './produkdetail.html', './kontak.html', './implementasi.html',
-     './pupuk-terbaik-untuk-hasil-panen-yang-optimal-di-Sumatera-Utara.html', './apakah-pupuk-aman-untuk-sayuran.html', './tes.html'
+     './artikel.html', './detail-artikel.html', './produkdetail.html', './kontak.html', './implementasi.html', './linkt.html',
+     './apakah-pupuk-aman-untuk-sayuran-yuk-cari-tahu.html', './tips-pemupukan-efektif-untuk-meningkatkan-hasil-panen.html',
+     './peran-pupuk-NPK-dalam-meningkatkan-produktivitas-lahan-pertanian.html', './panduan-memilih-pupuk-yang-tepat-untuk-tanaman-padi-dan-palawija.html'
   ],
   theme: {
     extend: {
