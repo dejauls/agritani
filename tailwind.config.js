aa1609e50406cc55ef1c2e5b2b6fa6dfae7378cd
dejauls <rajauls178@gmail.com>
2024-10-12 20:50:38 +0700

    clickbait judul artikel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: ['./index.html', './about.html', './brosur.html', './produk.html',
      './artikel.html', './detail-artikel.html', './produkdetail.html', './kontak.html', './implementasi.html', './linkt.html',
-     './apakah-pupuk-aman-untuk-sayuran-yuk-cari-tahu.html', './tips-pemupukan-efektif-untuk-meningkatkan-hasil-panen.html',
-     './peran-pupuk-NPK-dalam-meningkatkan-produktivitas-lahan-pertanian.html', './panduan-memilih-pupuk-yang-tepat-untuk-tanaman-padi-dan-palawija.html'
+     './benarkah-pupuk-aman-untuk-sayuran-fakta-mengejutkan-yang-haruskamu-tahu.html', './terungkap-cara-pemupukan-ajaib-yang-bikin-petani-panen-banyak.html',
+     './petani-sukses-bongkar-rahasia-pupuk-NPK-bikin-lahan-pertanian-jadi-super-produktif-tanpa-ribet.html', './rahasia-terbongkar-cara-memilih-pupuk-yang-bikin-padi-dan-palawija-panen-super-melimpah.html'
   ],
   theme: {
     extend: {
