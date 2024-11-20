031cd823514a6366bedd6c0fe5d148f442192c58
dejauls <rajauls178@gmail.com>
2024-11-21 00:47:24 +0700

    nambah 4 artikel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   content: ['./index.html', './about.html', './brosur.html', './produk.html',
      './artikel.html', './detail-artikel.html', './produkdetail.html', './kontak.html', './implementasi.html', './linkt.html',
-     './benarkah-pupuk-aman-untuk-sayuran-fakta-mengejutkan-yang-haruskamu-tahu.html', './terungkap-cara-pemupukan-ajaib-yang-bikin-petani-panen-banyak.html',
-     './petani-sukses-bongkar-rahasia-pupuk-NPK-bikin-lahan-pertanian-jadi-super-produktif-tanpa-ribet.html', './rahasia-terbongkar-cara-memilih-pupuk-yang-bikin-padi-dan-palawija-panen-super-melimpah.html'
+     './artikel/**/*.html',
   ],
   theme: {
     extend: {
